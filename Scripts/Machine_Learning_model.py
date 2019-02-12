1f0ae2f67eead39fc4ef2f6fd47a6e64e0f76a9f
Herman999 <jirman.tomas@gmail.com>
2019-02-12 14:11:06 +0000

    pytorch

--- Scripts/Machine_Learning_model.py
+++ Scripts/Machine_Learning_model.py
@@ -10,64 +10,60 @@ Created on Mon Feb 11 13:45:58 2019
 # dummpy preparation
 
 
-import pandas as pd
+#%%
+
+
+import torch 
+from torch.utils.data import TensorDataset, DataLoader
+import torch.nn.functional as F
+import torch.nn as nn
 import numpy as np
-import torch
+from torch.autograd import Variable
+
+inputs = np.array([np.linspace(0,100,10),np.linspace(0,10,10)]).T
+targets = inputs[:,0] + inputs[:,1]
+
+# transform to tensors
+inputs = torch.from_numpy(inputs).float()
+targets = torch.from_numpy(targets).float()
+
+
+train_ds = TensorDataset(inputs, targets) # careful about T
+batch_size = 5
+train_dl = DataLoader(train_ds, batch_size, shuffle=True)
+
+
+model = nn.Linear(2, 1)
 
 
 
-class LinearRegressionModel(torch.nn.Module): 
-  
-    def __init__(self): 
-        super(LinearRegressionModel, self).__init__() 
-        self.linear = torch.nn.Linear(3, 1)  # One in and one out 
-  
-    def forward(self, x): 
-        y_pred = self.linear(x) 
-        return y_pred 
+# Define optimizer
+opt = torch.optim.SGD(model.parameters(), lr=1e-5)
+
+
+# Define loss function
+loss_fn = F.mse_loss
+
+
+# Define a utility function to train the model
+def fit(num_epochs, model, loss_fn, opt):
+    for epoch in range(num_epochs):
+        for xb,yb in train_dl:
+            # Generate predictions
+            pred = model(xb)
+            #print(pred.T)
+            #print(yb)
+            loss = loss_fn(pred.squeeze(), yb)
+            # Perform gradient descent
+            loss.backward()
+            opt.step()
+            opt.zero_grad()
+    print('Training loss: ', loss_fn(model(inputs).squeeze(), targets))
+    
+fit(1000, model, loss_fn, opt)
 
 
 
-df = pd.DataFrame(data = [np.random.random(10),range(10), np.linspace(50,100,10)])
-df = df.T
-# df = x data
-
-x_data = torch.tensor(df.values)
-
-df[3] = df[2]
-y_data = torch.tensor(df[3].values.reshape(10,1))
-
-
-model = LinearRegressionModel()
-model.double()
-
-criterion = torch.nn.MSELoss(size_average = False) 
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.01) 
-
-
-
-for epoch in range(50): 
-  
-    # Forward pass: Compute predicted y by passing  
-    # x to the model 
-    pred_y = model(x_data) 
-  
-    # Compute and print loss 
-    loss = criterion(pred_y, y_data) 
-  
-    # Zero gradients, perform a backward pass,  
-    # and update the weights. 
-    optimizer.zero_grad() 
-    loss.backward() 
-    optimizer.step() 
-    print('epoch {}, loss {}'.format(epoch, loss.data[0])) 
-
-
-
-new_var = torch.autograd.Variable(torch.Tensor(np.array([100.,100.,100.])))
-model = model.float() 
-pred_y = model(new_var) 
-print("predict (after training)", 4, model(new_var).data[0][0]) 
 
 
 
